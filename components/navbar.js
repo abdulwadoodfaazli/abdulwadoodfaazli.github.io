234e7877b7963506f9fee7f272a6991d604dfba8
Abdulwadood Ashraf Faazli <awaf2001@connect.hku.hk>
2022-09-21 03:10:24 +0800

    initial

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+import { Image, Flex, Button, HStack, chakra } from "@chakra-ui/react";
+import { Link } from "react-scroll";
+import data from "./header.data";
+import React from "react";
+
+const CTA = "Get Started";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
